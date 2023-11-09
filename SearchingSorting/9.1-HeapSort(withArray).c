9dfcd5166bc9808d8df33243410910e4ad726309
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2023-11-09 22:51:42 +0530

    Add files via upload

---
+++ SearchingSorting/9.1-HeapSort(withArray).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct student{
+    char name[10];
+    int roll_no;
+    float cpi;
+}student;
+
+int main(){
+    student arr[10];
+}
